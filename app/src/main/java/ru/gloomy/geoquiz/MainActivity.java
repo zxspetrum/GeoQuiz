5ee79911954da1a49453ffcb387d890d7411e7cb
zxspetrum <ivladimirovich18@gmail.com>
2020-10-19 22:11:07 +0300

    “добавлена_анимация_лого”

--- app/src/main/java/ru/gloomy/geoquiz/MainActivity.java
+++ app/src/main/java/ru/gloomy/geoquiz/MainActivity.java
@@ -3,6 +3,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -19,6 +21,8 @@ public class MainActivity extends Activity {
         but_menu_resul = findViewById(R.id.but_menu_result);
         but_menu_test = findViewById(R.id.but_menu_test);
         iv_menu = findViewById(R.id.iv_menu);
+        final Animation animationRotateMenu = AnimationUtils.loadAnimation(
+                this, R.anim.rotate_menu);
 
         View.OnClickListener  allBut = new View.OnClickListener() {
             @Override
@@ -36,15 +40,17 @@ public class MainActivity extends Activity {
                         but_menu_resul.setBackgroundResource(R.drawable.but_menu_press);
                         Intent result = new Intent(MainActivity.this, ResultActivity.class);
                         startActivity(result);
-                        //break;
-
-
-                    //место для пасхалки
-                    case R.id.iv_menu:
-                        iv_menu.animate().rotation(-360);
                         break;
 
-
+                    case R.id.iv_menu:
+                        iv_menu.startAnimation(animationRotateMenu);
+                        break;
+                    //место для пасхалки
+/*
+Нажав на картинку определенное количество раз(азбука морзе),
+картинка меняется на грам.пластинку.Удерживая нажатием на изображении, пластинка вращяется
+играет музыка, по завершению проигрывания открывается  MOD режим
+ */
                 }
             }
         };

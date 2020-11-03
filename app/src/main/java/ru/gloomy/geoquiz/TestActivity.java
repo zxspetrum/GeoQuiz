299eef9616ad963fd7ab6b57147a844f9a7c99ff
zxspetrum <ivladimirovich18@gmail.com>
2020-11-03 16:44:55 +0300

    “03.11_new”

--- app/src/main/java/ru/gloomy/geoquiz/TestActivity.java
+++ app/src/main/java/ru/gloomy/geoquiz/TestActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +31,7 @@ public class TestActivity extends AppCompatActivity {
 
     List<String> quizQuestions;
     AdapterRecyclerView adapter;
+    TextView tvQuestion;
     int currentQuestion = 0;
     int correct = 0, wrong = 0;
 
@@ -39,22 +41,8 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        //get all the questions
-        loadAllQuestions();
-
-        //shuffle the questions if you want
-        Collections.shuffle(quizQuestions);
-
-        //загрузка первого вопроса
-        setQuestionScreen(currentQuestion);
-
-        RecyclerView rvAnswers = findViewById(R.id.rvAnswers);
-        rvAnswers.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new AdapterRecyclerView(this, quizQuestions);
-        rvAnswers.setAdapter(adapter);
-
         // создаем библиотеку
-        private void loadAllQuestions () {
+
             quizQuestions = new ArrayList<>();
             Gson gson = new Gson();
             InputStream fileInputStream = getResources().openRawResource(R.raw.question_qeoquiz);
@@ -62,28 +50,13 @@ public class TestActivity extends AppCompatActivity {
             QuestionList list = gson.fromJson(file, QuestionList.class);
             Log.e("TAG", "onCreate: " + list.getQuizQuestions().get(0).getQuestion());
 
-            try {
-                JSONObject jsonObj = new JSONObject(j);
-                JSONArray questions = jsonObj.getJSONArray("questions");
-                for (int i = 0; i < questions.length(); i++) {
-                    JSONObject question = questions.getJSONObject(i);
 
-                    String questionString = question.getString("question");
-                    String answerString = question.getString("answers");
-                    long true_answer = question.getLong("true_answer");
-                    String variant = question.getString("variant");
-                    quizQuestions.add(new QuizQuestion(
-                            questionString,
-                            answerString,
-                            true_answer,
-                            variant
-                    ));
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
+        RecyclerView rvAnswers = findViewById(R.id.rvAnswers);
+        TextView tvQuestion = findViewById(R.id.tvQuestion);
+        rvAnswers.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AdapterRecyclerView(this, quizQuestions);
+        rvAnswers.setAdapter(adapter);
+        tvQuestion.setText("Добавить вопрос из QuizQuestion");
     }
 
         public String readTextFile (InputStream inputStream){ // ввод чтение gson
@@ -103,36 +76,7 @@ public class TestActivity extends AppCompatActivity {
 
         }
 
-    public void onItemClick(View view, int position) {
-        if (quizQuestions.get(currentQuestion).getAnswer()
-                        .equals(quizQuestions.get(currentQuestion).getTrue_answer())) {
-                    //correct
-                    correct++;
-                    Toast.makeText(TestActivity.this, "Ответ правильный!", Toast.LENGTH_SHORT).show();
-                } else {
-                    //wrong
-                    wrong++;
-                    Toast.makeText(TestActivity.this, "Ответ неправильный! Правильный ответ: "
-                            + quizQuestions.get(currentQuestion).getTrue_answer(), Toast.LENGTH_SHORT).show();
-                }
 
-                //загружаем следующий вопрос
-                if (currentQuestion < quizQuestions.size() - 1) {
-                    currentQuestion++;
-                    setQuestionScreen(currentQuestion);
-                } else {
-                    //конец игры
-                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-                    intent.putExtra("correct: ", correct);
-                    intent.putExtra("wrong: ", wrong);
-                    startActivity(intent);
-                    finish();
-                }
 
-        });
-    private void setQuestionScreen(int number) {        //отображение вопросов на экране
-        tvQuestion.setText(questionItems.get(number).getQuestion());
-        rvAnswers.set(qquizQuestions.get(number).getAnswer());
-
-    }
+}
 
